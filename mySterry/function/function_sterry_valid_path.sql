21eb66a9a0c61b748b40323f79397dadd3db1bf5
[ progorker ] Software Development as a Platform <zinospetrel@sdf.org>
2026-06-05 19:20:39 +0700

    Add files via upload

---
+++ mySterry/function/function_sterry_valid_path.sql
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026 Dinh Thoai Tran <zinospetrel@sdf.org>
+ * All rights reserved.
+ *
+ * + Source URL: https://github.com/progorker/mysterry/
+ *
+ * + License: GPL-2.0
+ */
+
+drop function if exists sterry_valid_path;
+delimiter $$
+create function sterry_valid_path( p_input varchar(8192) )
+returns int
+deterministic
+sql security invoker
+begin
+  declare v_output varchar(8192);
+  if p_input is null then
+    return 0;
+  else
+    set v_output = trim( p_input );
+  end if;
+  if length( v_output ) = 0 then
+    return 0;
+  end if;
+  return 1;
+end;$$
+delimiter ;
