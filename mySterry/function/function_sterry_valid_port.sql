00b721ddc30970d1a6cd3e2d64303472a6ebebf1
[ progorker ] Zino's Petrel <zinospetrel@sdf.org>
2026-06-12 08:36:03 +0700

    Add files via upload

--- mySterry/function/function_sterry_valid_port.sql
+++ mySterry/function/function_sterry_valid_port.sql
@@ -2,7 +2,7 @@
  * Copyright (c) 2026 Dinh Thoai Tran <zinospetrel@sdf.org>
  * All rights reserved.
  *
- * + Source URL: https://github.com/progorker/mysterry/
+ * + Source URL: https://github.com/progorker/pgk_mysterry/
  *
  * + License: GPL-2.0
  */
